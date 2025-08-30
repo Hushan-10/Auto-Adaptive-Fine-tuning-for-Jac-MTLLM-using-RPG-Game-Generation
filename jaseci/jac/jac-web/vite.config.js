b1b03545a97f632704090c62fe1312a821fbdb55
Sethna Amanethmi <amanethmeis@gmail.com>
2025-08-30 21:17:02 +0530

    Update vite.config.js

--- jaseci/jac/jac-web/vite.config.js
+++ jaseci/jac/jac-web/vite.config.js
@@ -1,32 +1,3 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// //   server: {
-// //     port: 5173,
-// //     proxy: {
-// //       '/api': 'http://localhost:8000',
-// //       '/ws': { target: 'ws://localhost:8000', ws: true },
-// //     },
-// //   },
-// // })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     proxy: {
-//       '/api': 'http://localhost:8000',   // REST only
-//     },
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
